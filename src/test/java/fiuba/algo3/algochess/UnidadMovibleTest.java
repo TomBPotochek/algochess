18d955f992a89ed32b2e9c9eac8b15b2692638ee
Mariano Ramos <mariano.ramos@certagis.com>
2019-11-06 12:05:16 -0300

    Pruebas unidad movible

--- src/test/java/fiuba/algo3/algochess/UnidadMovibleTest.java
+++ src/test/java/fiuba/algo3/algochess/UnidadMovibleTest.java
@@ -13,30 +13,49 @@ public class UnidadMovibleTest {
     
 	@Test
     public void test01UnidadMoviblePuedeMoverseEnTodasLasDirecciones() {
-        Posicion posFinal = new Posicion(3,3);
-		
+		Posicion posInicialMock = mock(Posicion.class);
+        Posicion posFinalMock = mock(Posicion.class);
 		Tablero tableroMock = mock(Tablero.class);
 
-        Curandero unidad = new Curandero(new Posicion(2,3));
+		when(posInicialMock.esAdyacente(posFinalMock)).thenReturn(true);
 
-        unidad.mover(new Posicion(3,3), tableroMock);
-        assertTrue(posFinal.equals(unidad.getPosicion()));
+        Curandero unidad = new Curandero(posInicialMock);
+
+        unidad.mover(posFinalMock, tableroMock);
+        assertTrue(posFinalMock.equals(unidad.getPosicion()));
     }
     
-	/*
-	 * @Test public void test02UnidadMovibleNoPuedeMoverseAUnCasilleroOcupado() {
-	 * 
-	 * Tablero tablero = new Tablero();
-	 * 
-	 * 
-	 * UnidadMovible curandero = new Curandero(new Posicion(4,6)); UnidadMovible
-	 * jinete = new Jinete(new Posicion(4,5));
-	 * 
-	 * 
-	 * assertThrows(CasillaOcupadaException.class, () -> {
-	 * 
-	 * curandero.mover(new Posicion(4,5), tablero);
-	 * 
-	 * }); }
-	 */
+    @Test
+    public void test02UnidadMovibleNoPuedeMoverseAUnCasilleroOcupado() {
+    	
+    	Posicion posInicialMock = mock(Posicion.class);
+        Posicion posFinalMock = mock(Posicion.class);    
+    	Tablero tableroMock = mock(Tablero.class);
+    	UnidadMovible jinete = new Jinete(posInicialMock);
+    	
+    	when(posInicialMock.esAdyacente(posFinalMock)).thenReturn(true);
+    	doThrow(CasillaOcupadaException.class).when(tableroMock).moverUnidad(posInicialMock, posFinalMock);
+   
+    	assertThrows(CasillaOcupadaException.class, () -> {
+
+    		jinete.mover(posFinalMock, tableroMock);
+    	});	
+    }
+    
+    @Test
+    public void test03UnidadMovibleNoPuedeMoverseAUnCasilleroNoAdyacente() {
+
+    	Posicion posInicialMock = mock(Posicion.class);
+        Posicion posFinalMock = mock(Posicion.class);    
+    	Tablero tableroMock = mock(Tablero.class);
+    	UnidadMovible soldado = new Soldado(posInicialMock);
+    	
+    	when(posInicialMock.esAdyacente(posFinalMock)).thenReturn(false);
+   
+    	assertThrows(MovimientoInvalidoException.class, () -> {
+    	
+    		soldado.mover(posFinalMock, tableroMock);
+    	});	
+
+    }
 }
