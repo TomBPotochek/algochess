113935bbf9a916b8a2c2afde5d65355bc17c5e73
FranVinas <fvinas@fi.uba.ar>
2019-11-06 22:54:46 -0300

    jugador puede comprar unidades

--- src/main/java/fiuba/algo3/algochess/Jugador.java
+++ src/main/java/fiuba/algo3/algochess/Jugador.java
@@ -1,7 +1,50 @@
-package fiuba.algo3.algochess;
+package main.java.fiuba.algo3.algochess;
+
+import javafx.scene.control.Tab;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Jugador {
 	
 	private int puntos;
-	
+	private Set<Unidad> unidades;
+
+	public Jugador() {
+		puntos = 20;
+		unidades = new HashSet<>();
+	}
+
+	private boolean tienePuntosSuficientes(int costo) {
+		return puntos >= costo;
+	}
+
+	public void guardarUnidad(Tablero tablero, Unidad unidad, Posicion posicion) {
+		if(!this.tienePuntosSuficientes(unidad.obtenerCosto())) throw new PuntosInsuficientesException();
+		unidades.add(unidad);
+		tablero.colocarUnidad(unidad, posicion);
+	}
+
+	public void comprarSoldado(Tablero tablero, Posicion posicion) {
+		Soldado soldado = new Soldado(posicion);
+		this.guardarUnidad(tablero, soldado, posicion);
+	}
+
+	public void comprarJinete(Tablero tablero, Posicion posicion) {
+		Jinete jinete = new Jinete(posicion);
+		this.guardarUnidad(tablero, jinete, posicion);
+	}
+
+	public void comprarCurandero(Tablero tablero, Posicion posicion) {
+		Curandero curandero = new Curandero(posicion);
+		this.guardarUnidad(tablero, curandero, posicion);
+	}
+
+	public void comprarCatapulta(Tablero tablero, Posicion posicion) {
+		Catapulta catapulta = new Catapulta(posicion);
+		this.guardarUnidad(tablero, catapulta, posicion);
+	}
+
+
+
 }
