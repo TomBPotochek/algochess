9574e160e56e0f9bf60651a4fa5792717589939f
Tom <43979399+TomBPotochek@users.noreply.github.com>
2019-11-18 22:32:50 -0300

    se cambia Direccion a un enum

--- src/main/java/fiuba/algo3/algochess/Direccion.java
+++ src/main/java/fiuba/algo3/algochess/Direccion.java
@@ -2,6 +2,28 @@ package fiuba.algo3.algochess;
 
 import fiuba.algo3.algochess.juego.Posicion;
 
-public interface Direccion {
-    Posicion calcularSiguientePosicion(Posicion posicion);
+public enum Direccion {
+	N(0,1),
+	NE(1,1),
+	E(1,0),
+	SE(1,-1),
+	S(0,-1),
+	SO(-1,-1),
+	O(-1,0),
+	NO(-1,1);
+	
+	private int dx;
+	private int dy;
+	Direccion(int dx, int dy) {
+		this.dx = dx;
+		this.dy = dy;
+	}
+	
+	public Posicion calcularPosicionSiguiente(Posicion posicion) {
+		int x = posicion.obtenerCoordenadaX();
+		int y = posicion.obtenerCoordenadaY();
+		x += this.dx;
+		y += this.dy;
+		return new Posicion(x,y);
+	}
 }
