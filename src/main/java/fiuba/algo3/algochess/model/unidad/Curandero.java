a0318d2d81c3c9f111f9cae52a264563b0a7efe2
SantiagoTadini <santi.tadini@gmail.com>
2019-11-27 16:38:14 -0300

    mas comentarios borrados

--- src/main/java/fiuba/algo3/algochess/model/unidad/Curandero.java
+++ src/main/java/fiuba/algo3/algochess/model/unidad/Curandero.java
@@ -13,19 +13,17 @@ public class Curandero extends UnidadMovible {
     public Curandero(Posicion unaPosicion, Equipo equipo) {
     	super(unaPosicion, equipo);
 
-    	//this.curacion = CURACION_CURANDERO;
     	this.vidaInicial = VIDA_CURANDERO;
     	this.vidaRestante = vidaInicial;
     	this.costo = COSTO_CURANDERO;
     	this.arma = new Medikit();
     }
 
-    @Override //hay que tener en cuenta que el curar de curandero es atacar, pero no puede curar enemigos
+    @Override
 	public void atacar(Unidad unidad, Tablero tablero) {
 		Distancia dist = this.posicion.calcularDistancia(unidad.obtenerPosicion());
 		if (unidad.esAliada(this.equipo))
 			dist.atacar(unidad, this.arma);
-		//else throw new Exception(); //casillaEnemigaException?
 	}
 
 }
--- src/main/java/fiuba/algo3/algochess/model/unidad/Espada.java
+++ src/main/java/fiuba/algo3/algochess/model/unidad/Espada.java
@@ -23,7 +23,4 @@ public class Espada implements Arma {
 		throw new AtaqueInvalidoException();
 		
 	}
-
-	
-
 }
